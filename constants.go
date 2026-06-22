94670e426a718a9619598e4f6a5e3219ac00aa0a
sofa <sophiapark0@proton.me>
2026-06-22 02:09:01 -0700

    Change URLs to avoid "updating" to the normal version

--- constants.go
+++ constants.go
@@ -13,10 +13,10 @@ import (
 
 const ReleaseUrl = "https://api.github.com/repos/Vendicated/Vencord/releases/latest"
 const ReleaseUrlFallback = "https://vencord.dev/releases/vencord"
-const InstallerReleaseUrl = "https://api.github.com/repos/Vencord/Installer/releases/latest"
+const InstallerReleaseUrl = "https://api.github.com/repos/comfiestsofa/VencordInstaller/releases/latest"
 const InstallerReleaseUrlFallback = "https://vencord.dev/releases/installer"
 
-var UserAgent = "VencordInstaller/" + buildinfo.InstallerGitHash + " (https://github.com/Vencord/Installer)"
+var UserAgent = "VencordInstaller/" + buildinfo.InstallerGitHash + " (https://github.com/comfiestsofa/VencordInstaller)"
 
 var (
 	DiscordGreen  = color.RGBA{R: 0x2D, G: 0x7C, B: 0x46, A: 0xFF}

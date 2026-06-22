86c2e0f2b58923b32fbcf339da489ea71e252612
sofa <sophiapark0@proton.me>
2026-06-22 02:15:32 -0700

    fix installer scripts, sh now works on macos and windows

--- install.sh
+++ install.sh
@@ -18,25 +18,44 @@ echo "Downloading Installer..."
 
 set -- "XDG_CONFIG_HOME=$XDG_CONFIG_HOME"
 
-curl -sS https://github.com/Vendicated/VencordInstaller/releases/latest/download/VencordInstallerCli-Linux \
-  --output "$outfile" \
-  --location \
-  --fail
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  curl -sS https://github.com/comfiestsofa/VencordInstaller/releases/latest/download/VencordInstallerCli-macOS \
+    --output "$outfile" \
+    --location \
+    --fail
 
-chmod +x "$outfile"
+  "$outfile"
+elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+  # https://stackoverflow.com/a/8597411
+  curl -sS https://github.com/comfiestsofa/VencordInstaller/releases/latest/download/VencordInstallerCli.exe \
+    --output "$outfile" \
+    --location \
+    --fail
 
-if command -v sudo >/dev/null; then
-  echo "Running with sudo"
-  sudo env "$@" "$outfile"
-elif command -v doas >/dev/null; then
-  echo "Running with doas"
-  doas env "$@" "$outfile"
-elif command -v run0 >/dev/null; then
-  echo "Running with run0"
-  run0 env "$@" "$outfile"
-elif command -v pkexec >/dev/null; then
-  echo "Running with pkexec"
-  pkexec env "$@" "SUDO_USER=$(whoami)" "$outfile"
+  "$outfile"
 else
-  echo "Neither sudo nor doas were found. Please install either of them to proceed."
+  curl -sS https://github.com/comfiestsofa/VencordInstaller/releases/latest/download/VencordInstallerCli-Linux \
+    --output "$outfile" \
+    --location \
+    --fail
+
+  chmod +x "$outfile"
+
+  if command -v sudo >/dev/null; then
+    echo "Running with sudo"
+    sudo env "$@" "$outfile"
+  elif command -v doas >/dev/null; then
+    echo "Running with doas"
+    doas env "$@" "$outfile"
+  elif command -v run0 >/dev/null; then
+    echo "Running with run0"
+    run0 env "$@" "$outfile"
+  elif command -v pkexec >/dev/null; then
+    echo "Running with pkexec"
+    pkexec env "$@" "SUDO_USER=$(whoami)" "$outfile"
+  else
+    echo "Neither sudo nor doas were found. Please install either of them to proceed."
+  fi
 fi
+
+exit
